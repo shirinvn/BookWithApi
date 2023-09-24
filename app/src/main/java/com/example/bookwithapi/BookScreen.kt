3f08da2da678018b324c465dfddd488167540e66
Vatani <shirinvatani97@gmail.com>
2023-09-25 00:01:01 +0330

    Commit

--- app/src/main/java/com/example/bookwithapi/BookScreen.kt
+++ app/src/main/java/com/example/bookwithapi/BookScreen.kt
@@ -1,6 +1,7 @@
 package com.example.bookwithapi
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -12,9 +13,12 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview
 @Composable
 fun CaptionScreen(){
-    Column(modifier = Modifier.fillMaxSize().background(Color.Red)) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Red)) {
 
 
+        Box(modifier = Modifier.fillMaxSize())
     }
 
 
